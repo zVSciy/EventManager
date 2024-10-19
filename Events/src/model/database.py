2ae6d38298765477e8639b8fa803e9a9ccc55d9f
pintere6 <pintere@edu.htl-villach.at>
2024-10-19 14:48:17 +0200

    Containerization

--- Events/src/model/database.py
+++ Events/src/model/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+user = os.environ.get("MYSQL_USER")
+hostname = os.environ.get("MYSQL_HOST")
+port = os.environ.get("MYSQL_DATABASE_PORT")
+password = os.environ.get("MYSQL_ROOT_PASSWORD")
 
 # Database configuration
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/EventManagement"
+DATABASE_URL = f"mysql+pymysql://root:{password}@db:{port}/EventManagement"
+print(DATABASE_URL)
 
 # Initialize database connection
 engine = create_engine(DATABASE_URL)
