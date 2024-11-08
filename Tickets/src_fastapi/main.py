4a64e21ffeccb3290f895f1a267569d1350daf5a
romauchd <daniel.romauch@edu.htl-villach.at>
2024-11-08 13:12:02 +0100

    Tickets - GET /notes

--- Tickets/src_fastapi/main.py
+++ Tickets/src_fastapi/main.py
@@ -7,5 +7,14 @@ from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-# @app.get("/tickets")
-# ...
+@app.get("/tickets")
+def read_tickets(event_id: None, db: Session = Depends(get_db))
+    if user_id:
+        tickets = db.query(models.Ticket).filter(models.Ticket.event_id == event_id).all()
+    else:
+        tickets = db.query(models.Ticket).all()
+    return tickets
+
+@app.post("/tickets")
+def add_ticket(ticket:TicketInput db: Session = Depends(get_db)):
+    pass
