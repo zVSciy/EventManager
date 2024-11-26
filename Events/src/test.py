56deba77ebc72821943316c82e423b26a5f928d0
pintere <pintere@edu.htl-villach.at>
2024-11-26 06:51:34 +0100

    Updated tests

--- Events/src/test.py
+++ Events/src/test.py
@@ -14,21 +14,15 @@ class TestEventAPI(unittest.TestCase):
     def test_get_event(self):
         event_id = 1  
         response = client.get(f"/event/{event_id}")
-        if response.status_code == 404:
-            self.assertEqual(response.json(), {"detail": "Event not found"})
-        else:
-            self.assertEqual(response.status_code, 200)
-            self.assertIn("ID", response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("ID", response.json())
 
     def test_get_event_tickets(self):
         event_id = 1  
         response = client.get(f"/event/available_tickets/{event_id}")
-        if response.status_code == 404:
-            self.assertEqual(response.json(), {"detail": "Event not found"})
-        else:
-            self.assertEqual(response.status_code, 200)
-            self.assertIn("available_normal_tickets", response.json())
-            self.assertIn("available_vip_tickets", response.json())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("available_normal_tickets", response.json())
+        self.assertIn("available_vip_tickets", response.json())
 
     def test_create_event(self):
         event_data = {
@@ -45,40 +39,31 @@ class TestEventAPI(unittest.TestCase):
         self.assertEqual(response.json()["response"], "Event was created successfully")
 
     def test_update_event(self):
-        event_id = 1 
+        event_id = 1
         updated_event_data = {
             "name": "Updated Concert Name",
             "location": "New Stadium"
         }
         response = client.put(f"/event/{event_id}/", json=updated_event_data)
-        if response.status_code == 404:
-            self.assertEqual(response.json(), {"detail": "Event not found"})
-        else:
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["response"], "Event was updated successfully")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["response"], "Event was updated successfully")
 
     def test_cancel_event(self):
         event_id = 1 
         cancel_data = {"canceled": True}
         response = client.put(f"/event/cancel/{event_id}", json=cancel_data)
-        if response.status_code == 404:
-            self.assertEqual(response.json(), {"detail": "Event not found"})
-        else:
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["response"], "Event was canceled successfully")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["response"], "Event was canceled successfully")
 
     def test_update_event_tickets(self):
-        event_id = 1  
+        event_id = 1
         ticket_update_data = {
             "available_normal_tickets": 80,
             "available_vip_tickets": 40
         }
         response = client.put(f"/event/updateTicket/{event_id}", json=ticket_update_data)
-        if response.status_code == 404:
-            self.assertEqual(response.json(), {"detail": "Event not found"})
-        else:
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json()["response"], "Available tickets were updated successfully")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()["response"], "Available tickets were updated successfully")
 
 if __name__ == "__main__":
     unittest.main()
