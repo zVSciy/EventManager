4d90e4f72e816dbc8858bbe048148d4f876b6e40
zVSciy <manager.fallen0815@gmail.com>
2025-01-24 10:17:36 +0100

    fix: update entry point in Dockerfile and remove subprocess call in app.py

--- Review/frontend/app.py
+++ Review/frontend/app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    subprocess.run(["python", "flask_app/create_json.py"])
     return render_template("index.html")
 
 if __name__ == "__main__":
