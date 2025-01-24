3f198e39455bc760f91f2b5587b9ae3f0382e981
zVSciy <manager.fallen0815@gmail.com>
2025-01-24 09:49:12 +0100

    added frontend service

---
+++ Review/frontend/app.py
@@ -0,0 +1,14 @@
+import subprocess
+
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    subprocess.run(["python", "flask_app/create_json.py"])
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
