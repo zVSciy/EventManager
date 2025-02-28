fb9bb432e140e3eebe21191a8c683d8d8177d1cd
pl0nr <fabian.ploner05@gmail.com>
2025-02-28 11:01:54 +0000

    UserManagement - updating +server.js for register endpoint

--- UserManagement/frontend/src/routes/api/register/+server.js
+++ UserManagement/frontend/src/routes/api/register/+server.js
@@ -1,18 +1,39 @@
-import { json } from '@sveltejs/kit';
+import { error } from "@sveltejs/kit";
+
+const API_BASE_URL = 'http://backend:8000';
 
 export async function POST({ request }) {
+    const { email, password, first_name, last_name, role } = await request.json();
+
     try {
-        const { email, password, first_name, last_name, role } = await request.json();
-        
-        // Simulate user registration
-        const user = { email, first_name, last_name, role };
+        const response = await fetch(`${API_BASE_URL}/register`, {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: email,
+                hashed_password: password,
+                first_name: first_name,
+                last_name: last_name,
+                role: role
+            })
+        });
 
-        // Simulate saving the user to the database
-        console.log('User registered:', user);
-
-        return json({ message: 'User registered successfully!', user });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        return json({ message: 'Internal Server Error' }, { status: 500 });
+        return jsonResponse(response, response.status);
+    } 
+    
+    catch (error) {
+        return jsonResponse({ message: "An error occurred", error }, 500);
     }
+}
+
+function jsonResponse(data, status) {
+    return new Response(JSON.stringify(data), {
+        status: status,
+        headers: {
+            "Content-Type": "application/json",
+        }
+    });
 }
